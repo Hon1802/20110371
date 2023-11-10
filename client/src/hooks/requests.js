eaab30b7f331491ca75a5e517a767ef0dbd695f7
Nguyễn Văn Hơn <74673759+Hon1802@users.noreply.github.com>
2023-11-10 22:13:22 +0700

    .
    .

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,58 @@
+const API_URL = 'http://localhost:8081';
+async function httpGetPlanets() {
+  const response = await fetch(`${API_URL}/planets`)
+  return await response.json()
+  // TODO: Once API is ready.
+  // Load planets and return as JSON.
+}
+
+async function httpGetLaunches() {
+  const response = await fetch(`${API_URL}/launches`)
+  const fetchLaunches = await response.json()
+  return fetchLaunches.sort((a,b)=>{
+    return a.flightNumber - b.flightNumber;
+  })
+  // TODO: Once API is ready.
+  // Load launches, sort by flight number, and return as JSON.
+}
+
+async function httpSubmitLaunch(launch) {
+  try{
+    return await fetch(`${API_URL}/launches`,{
+      method:"post",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify(launch),
+    })
+  }catch(e)
+  {
+    return {
+      ok:false,
+    };
+  }
+  // TODO: Once API is ready.
+  // Submit given launch data to launch system.
+}
+
+async function httpAbortLaunch(id) {
+  try{
+    return await fetch(`${API_URL}/launches/${id}`,{
+      method: "delete",
+    })
+  }catch(e)
+  {
+    return{
+      ok:false,
+    }
+  }
+  // TODO: Once API is ready.
+  // Delete launch with given ID.
+}
+
+export {
+  httpGetPlanets,
+  httpGetLaunches,
+  httpSubmitLaunch,
+  httpAbortLaunch,
+};
